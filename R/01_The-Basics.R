5245299d1552d98f041c14ef9a6676e88d9dd4df
grousell <grousell@gmail.com>
2022-03-17 11:00:46 -0400

    Update the survey open-responses.

--- R/01_The-Basics.R
+++ R/01_The-Basics.R
@@ -34,7 +34,10 @@ vec1 <- c ("a", "b", "c", "d", "e") # String/Character
 vec2 <- c (1,2,3,4,5) # Numeric
 
 # Apply functions to vectors as well
+vec2
 fun1(vec2)
+
+vec2
 fun2(vec2)
 
 # New function
@@ -43,18 +46,27 @@ fun3 <- function(x){
           ifelse (x == "b", "A", "B")
   )
 }
+
+vec1
 fun3 (vec1)
 
 # Factors - numeric assignment to nominal variable
 
 gender <- c ("Male", "Male", "Female", "Male", "Female")
+str(gender)
+
 gender <- factor (gender) # Now "Male" and "Female" are represented by numbers
+str(gender)
+levels (gender)
+
 as.numeric(gender)
 
 # Change the order of the factors
 gender <- factor (gender,
                   levels = c("Male", "Female"))
+levels (gender)
 as.numeric(gender)
+
 # Packages ----------------------------------------------------------------
 # http://www.tidyverse.org
 
@@ -70,24 +82,32 @@ library(tidyverse)
 
 # install.packages("tictoc")
 library(tictoc)
-# Tidyverse
-tic()
-df1 <- read_csv ("https://raw.githubusercontent.com/grousell/MISAR/master/Data/parking_data_all.csv") %>% #then
-  head(5000) # Selects first 5000 rows
-toc()
 
 # Base
 tic()
-df1 <- read.csv ("https://raw.githubusercontent.com/grousell/MISAR/master/Data/parking_data_all.csv",
+parking <- read.csv ("https://raw.githubusercontent.com/grousell/MISAR/master/Data/parking_data_all.csv",
                  stringsAsFactors = FALSE) %>%
   head(5000) # Selects first 5000 rows
 toc()
 
-# Import Data -------------------------------------------------------------
-
-# csv files
-parking <- read_csv ("https://raw.githubusercontent.com/grousell/MISAR/master/Data/parking_data_all.csv") %>%
+# Tidyverse
+tic()
+parking <- read_csv ("https://raw.githubusercontent.com/grousell/MISAR/master/Data/parking_data_all.csv") %>% #then
   head(5000) # Selects first 5000 rows
+toc()
+
+# Base R vs Tidyverse
+
+new_parking_base <- parking[which(parking$location1 == "OPP" & parking$infraction_code == 406),
+                       c("tag_number_masked","date_of_infraction", "infraction_description","location2" )]
+
+
+new_parking_tidy <- parking %>%
+  dplyr::filter (location1 == "OPP" & infraction_code == 406) %>%
+  select (tag_number_masked, date_of_infraction, infraction_description, location2)
+
+
+# Import Data -------------------------------------------------------------
 
 # SPSS
 library(foreign) # Foreign package will read in SPSS and .dbf files
@@ -98,32 +118,26 @@ df2 <- read.spss("file.sav")
 df3 <- read.dbf ("file.dbf",
                  as.is = TRUE) # Keeps as character vs. factors
 
-# Explore Data -----------------------------------------------------------
-
-str(df1)
-glimpse(df1)
-table (df$infraction_description)
 
 # Survey Data -------------------------------------------------------------
 # May have extraneous columns, or names may be too long.
-survey <- read_csv("https://raw.githubusercontent.com/grousell/MISAR/master/Data/survey_data.csv")
+survey <- read_csv("https://raw.githubusercontent.com/grousell/MISAR/master/Data/survey_data_1.csv")
 glimpse(survey)
 
 # Don't really need columns like CollectorID, IP Address
 # Columns names are quite long - can shorten them
 
-survey <- read_csv("https://raw.githubusercontent.com/grousell/MISAR/master/Data/survey_data.csv")  %>%
+survey <- read_csv("https://raw.githubusercontent.com/grousell/MISAR/master/Data/survey_data_1.csv")%>%
   # Select only columns what we need, and rename the columns
-  select (goal = "My goal for attending this session: - Open-Ended Response",
-          expert = "On this topic, I consider myself:",
-          useful = "In regards to this session, the content presented: - Is USEFUL to me",
-          applicable = "In regards to this session, the content presented: - Is APPLICABLE to my job",
-          changed = "In regards to this session, the content presented: - Has CHANGED my thinking",
-          reinforced = "In regards to this session, the content presented: - Has REINFORCED my thinking"
-          )
+  select (-StartDate, -EndDate) %>%
+  rename (goal = "What is your goal for attending this session?")
 
 # Examine dataframe
 glimpse(survey)
+
+survey %>%
+  select (RespondentID, school, consider_self) %>%
+  glimpse()
 
 # Quick analyses
 table (survey$useful)
